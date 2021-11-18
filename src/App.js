175ca7541ba17ce981f05861798471a52c78986b
Max <26414444+maxinnes@users.noreply.github.com>
2021-11-18 20:22:19 +0000

    Convert to component
    in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  return (
-      <h1>Sample text</h1>
-  );
+import React from "react";
+// Import PageComponents
+
+
+class App extends React.Component{
+  render() {
+    return <div className="container mt-4">
+      <div className="row">
+
+      </div>
+      <div className="row mt-4">
+
+      </div>
+    </div>
+  }
 }
 
 export default App;
---
+++ src/PageComponents/HomeTop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class HomeTop extends React.Component{
+    render() {
+        return
+    }
+}
