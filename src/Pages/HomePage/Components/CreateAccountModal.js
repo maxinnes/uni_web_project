70fa706eaab1106d40220980ca176a784f190af3
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-02 12:16:46 +0000

    Big up form verification

--- src/Pages/HomePage/Components/CreateAccountModal.js
+++ src/Pages/HomePage/Components/CreateAccountModal.js
@@ -1,70 +1,73 @@
 import React from "react";
 
 class CreateAccountModal extends React.Component{
-    // firstNameInputValidation(){
-    //     let firstName = document.getElementById("registerFirstNameInput").value;
-    //     if(firstName.length===0){
-    //         this.setState(prevState =>{
-    //             let firstName = Object.assign({}, prevState.firstNameInput);
-    //             firstName.classNames.indexOf("is-invalid") === -1 ? firstName.classNames.push("is-invalid") : console.log("Class already applied")
-    //             firstName.invalidFeedback = "First name cannot be empty"
-    //             return { firstName };
-    //         })
-    //     } else{
-    //         this.setState(prevState =>{
-    //             let firstName = Object.assign({}, prevState.firstNameInput);
-    //             firstName.classNames.indexOf("is-invalid") > -1 ? firstName.classNames.splice(firstName.classNames.indexOf("is-invalid"),1) : console.log("Class already applied")
-    //             firstName.invalidFeedback = ""
-    //             return { firstName };
-    //         })
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.errorList = {
+            "firstNameError":false,
+            "lastNameError":false,
+            "emailNameError":false,
+            "passwordNameError":false,
+            "verifyPasswordNameError":false
+        }
+    }
+    nameValidation(event){
+        const nameInputElement = event.target
+        const name = nameInputElement.value
+        if(name.length===0) {
+            nameInputElement.classList.add("is-invalid")
+            nameInputElement.nextSibling.innerText = "First Name must be longer than 0"
+        }else if(!/^[a-zA-Z]+$/gm.test(name)){
+            nameInputElement.classList.add("is-invalid")
+            nameInputElement.nextSibling.innerText = "Invalid name syntax"
+        }else{
+            nameInputElement.classList.remove("is-invalid")
+        }
+    }
+    emailValidation(event){
+        const emailInputElement = event.target
+        const email = emailInputElement.value
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if(email.length===0){
+            emailInputElement.classList.add("is-invalid")
+            emailInputElement.nextSibling.innerText = "Email must be longer than 0"
+        }else if(!emailRegex.test(email)){
+            emailInputElement.classList.add("is-invalid")
+            emailInputElement.nextSibling.innerText = "Invalid email syntax"
+        }else {
+            emailInputElement.classList.remove("is-invalid")
+        }
+    }
+    passwordValidation(event){
+        document.getElementById("registerVerifyPasswordInput").value = ""
+        const passwordInputElement = event.target
+        const password = passwordInputElement.value
 
-    // submitFormDetails(){
-    //     // Get form details
-    //     let firstName = document.getElementById("registerFirstNameInput").value;
-    //     let lastName = document.getElementById("registerLastNameInput").value;
-    //     let email = document.getElementById("registerEmailInput").value;
-    //     let password = document.getElementById("registerPasswordInput").value;
-    //     let verifyPassword = document.getElementById("registerVerifyPasswordInput").value;
-    //
-    //     //if(password!==verifyPassword){
-    //         //throw new Error()
-    //     //}
-    //
-    //     // Create request
-    //     let myHeaders = new Headers();
-    //     myHeaders.append("Content-Type", "application/json");
-    //
-    //     let raw = JSON.stringify({
-    //         "firstName": firstName,
-    //         "lastName": lastName,
-    //         "email": email,
-    //         "password": password
-    //     });
-    //
-    //     let requestOptions = {
-    //         method: 'POST',
-    //         headers: myHeaders,
-    //         body: raw,
-    //         redirect: 'follow'
-    //     };
-    //
-    //     fetch("http://localhost/api/accounts/createAccount.php", requestOptions)
-    //         .then(response => response.text())
-    //         .then(result => console.log(result))
-    //         .catch(error => console.log('error', error));
-    // }
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         firstNameInput:{
-    //             classNames: ["form-control"],
-    //             invalidFeedback: ""
-    //         }
-    //     }
-    // }
-    
+        if(password.length < 5){
+            passwordInputElement.classList.add("is-invalid")
+            passwordInputElement.nextSibling.innerText = "Password must be longer then 5 characters."
+        }else{
+            passwordInputElement.classList.remove("is-invalid")
+        }
+    }
+    passwordVerifyValidation(event){
+        const password = document.getElementById("registerPasswordInput").value
+        const passwordVerifyElement = event.target
+        const passwordVerify = passwordVerifyElement.value
+        if(password!==passwordVerify){
+            passwordVerifyElement.classList.add("is-invalid")
+            passwordVerifyElement.nextSibling.innerText = "Passwords do not match."
+        }else{
+            passwordVerifyElement.classList.remove("is-invalid")
+        }
+    }
+    validateAndSubmitForm(){
+        const firstName = document.getElementById("registerFirstName").value
+        const lastName = document.getElementById("registerLastName").value
+        const email = document.getElementById("registerEmailName").value
+        const password = document.getElementById("registerPassword").value
+        const verifyPassword = document.getElementById("registerPassword").value
+    }
     render() {
         return <div className="modal fade" id="createAccountModal" tabIndex="-1" aria-labelledby="createAccountModal" aria-hidden="true">
             <div className="modal-dialog">
@@ -76,27 +79,28 @@ class CreateAccountModal extends React.Component{
                     <div className="modal-body">
                         <form noValidate className="needs-validation">
                             <div className="form-floating mt-3">
-                                <input required className="form-control" type="text" id="registerFirstNameInput" placeholder="First Name"/>
+                                <input onChange={(event)=>{this.nameValidation(event)}} required className="form-control" type="text" id="registerFirstNameInput" placeholder="First Name"/>
                                 <div className="invalid-feedback" />
                                 <label htmlFor="registerFirstNameInput">First Name</label>
                             </div>
                             <div className="form-floating mt-3">
-                                <input required className="form-control" type="text" id="registerLastNameInput" placeholder="Last Name"/>
+                                <input onChange={(event)=>{this.nameValidation(event)}} required className="form-control" type="text" id="registerLastNameInput" placeholder="Last Name"/>
+                                <div className="invalid-feedback" />
                                 <label htmlFor="RegisterLastNameInput">Last Name</label>
-                                <div className="invalid-feedback">
-                                    Last Name Required.
-                                </div>
                             </div>
                             <div className="form-floating mt-3">
-                                <input required className="form-control" type="email" id="registerEmailInput" placeholder="name@example.com"/>
+                                <input onChange={(event)=>{this.emailValidation(event)}} required className="form-control" type="email" id="registerEmailInput" placeholder="name@example.com"/>
+                                <div className="invalid-feedback"/>
                                 <label htmlFor="registerEmailInput">Email Address</label>
                             </div>
                             <div className="form-floating mt-3">
-                                <input required className="form-control" type="password" id="registerPasswordInput" placeholder="Password"/>
+                                <input onChange={(event)=>{this.passwordValidation(event)}} required className="form-control" type="password" id="registerPasswordInput" placeholder="Password"/>
+                                <div className="invalid-feedback"/>
                                 <label htmlFor="registerPasswordInput">Password</label>
                             </div>
                             <div className="form-floating my-3">
-                                <input required className="form-control" type="password" id="registerVerifyPasswordInput" placeholder="Verify Password"/>
+                                <input onChange={(event)=>{this.passwordVerifyValidation(event)}} required className="form-control" type="password" id="registerVerifyPasswordInput" placeholder="Verify Password"/>
+                                <div className="invalid-feedback"/>
                                 <label htmlFor="registerVerifyPasswordInput">Verify Password</label>
                             </div>
                         </form>
