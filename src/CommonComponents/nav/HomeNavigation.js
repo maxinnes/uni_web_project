ef53f9f7972d11ad5e65c31691f17fdeedfaf507
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-19 12:07:47 +0000

    Style fixes

--- src/CommonComponents/nav/HomeNavigation.js
+++ src/CommonComponents/nav/HomeNavigation.js
@@ -1,14 +1,18 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import styles from '../../scss/WelcomePages.module.css';
 
-function HomeNavigation(){
+// old class home-nav
+export default function HomeNavigation(){
+
     return <nav className="home-nav">
         <ul className="nav">
+            {/*<li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/">Home</NavLink></li>*/}
+            {/*<li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/pricing">Pricing</NavLink></li>*/}
+            {/*<li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/about">About</NavLink></li>*/}
             <li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/">Home</NavLink></li>
             <li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/pricing">Pricing</NavLink></li>
             <li className="nav-item mx-2"><NavLink activeClassName="active" className="nav-link" to="/about">About</NavLink></li>
         </ul>
     </nav>
 }
-
-export default HomeNavigation;
