826d9835896b8bd9404dc0f5edce1b802f0fe733
Max <26414444+maxinnes@users.noreply.github.com>
2021-12-23 21:26:23 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './scss/WelcomePages.module.css';
 import './scss/Bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
@@ -16,7 +15,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
